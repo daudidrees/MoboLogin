e94b2af2f8d79e03aa410e3cde38267106cba136
Husnain0707 <haseehk70086@gmail.com>
2023-01-09 15:41:25 +0500

    vnail updated

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function openCity(evt, cityName) {
-    var i, tabsection, tablinks;
-    tabsection = document.getElementsByClassName("tabsection");
-    for (i = 0; i < tabsection.length; i++) {
-      tabsection[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabsection, tablinks;
+  tabsection = document.getElementsByClassName("tabsection");
+  for (i = 0; i < tabsection.length; i++) {
+    tabsection[i].style.display = "none";
   }
-  
-  document.getElementById("defaultOpen").click();
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+
+function closeBtn (elem) {
+  document.getElementById(elem).classList.add('visibility-hidden');
+}
+
+function popups(funnc) {
+  document.getElementById(funnc).classList.remove('visibility-hidden');
+}
