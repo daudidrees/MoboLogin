7b1b21e2256833949e43475318a88ca9787ccab8
Husnain0707 <haseehk70086@gmail.com>
2023-01-13 03:12:47 +0500

    vnail slider pages updated

--- script.js
+++ script.js
@@ -25,6 +25,31 @@ window.onload = function () {
   }
 }
 
+const filterContainer = document.querySelector(".gallery-filter"),
+  galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("filter-item")) {
+    // deactivate existing active 'filter-item'
+    filterContainer.querySelector(".active").classList.remove("active");
+    // activate new 'filter-item'
+    event.target.classList.add("active");
+    const filterValue = event.target.getAttribute("data-filter");
+    galleryItems.forEach((item) => {
+      if (item.classList.contains(filterValue) || filterValue === 'all') {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      }
+      else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+    });
+  }
+});
+
+
+
 function openCity(evt, cityName) {
   var i, tabsection, tablinks;
   tabsection = document.getElementsByClassName("tabsection");
@@ -49,3 +74,4 @@ function closeBtn(elem) {
 function popups(funnc) {
   document.getElementById(funnc).classList.remove('visibility-hidden');
 }
+
